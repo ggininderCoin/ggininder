933cccfc024712b9e3470411282ccb210fa3baaf
ggininderCoin <83423908+ggininderCoin@users.noreply.github.com>
2021-05-01 03:44:18 +0800

    http to https

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,11 @@
     $(document).ready(function () {
         "use strict";
 
+        let targetProtocol = "https:";
+        if (window.location.protocol != targetProtocol) {
+            window.location.href = targetProtocol +
+                window.location.href.substring(window.location.protocol.length);
+        }
 
         // ICON CONTENT BLOCK
         $('.icon-content-block .content-block').mouseenter(function () {
